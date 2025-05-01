12d290a0cd28c1bbb44bc5844daa317878a6131b
Helena Oliveira <helena@informach.com.br>
2025-05-01 08:37:04 -0300

    add grid

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,39 @@
+import { CANVAS_SIZE, CELL_SIZE, GRID_SIZE } from './constants'
 import './style.css'
+
+class Game {
+  canvas: HTMLCanvasElement
+  ctx: CanvasRenderingContext2D
+
+  constructor() {
+    this.canvas = document.getElementById('game-canvas') as HTMLCanvasElement
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    this.ctx = this.canvas.getContext('2d')!
+    this.canvas.width = CANVAS_SIZE
+    this.canvas.height = CANVAS_SIZE
+
+    this.drawGrid()
+  }
+
+  drawGrid() {
+    this.ctx.fillStyle = '#2c3e50'
+    this.ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+
+    this.ctx.strokeStyle = '#ffffff33'
+    this.ctx.lineWidth = 1
+
+    for (let i = 0; i <= GRID_SIZE; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(i * CELL_SIZE, 0)
+      this.ctx.lineTo(i * CELL_SIZE, CANVAS_SIZE)
+      this.ctx.stroke()
+
+      this.ctx.beginPath()
+      this.ctx.moveTo(0, i * CELL_SIZE)
+      this.ctx.lineTo(CANVAS_SIZE, i * CELL_SIZE)
+      this.ctx.stroke()
+    }
+  }
+}
+
+window.onload = () => new Game()
